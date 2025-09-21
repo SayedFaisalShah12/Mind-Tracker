c487fed20ede492649b1e374ecec230dd6bd4a87
Sayed Faisal Shah <142516641+SayedFaisalShah12@users.noreply.github.com>
2025-09-21 23:58:59 +0500

    update

--- lib/views/app.dart
+++ lib/views/app.dart
@@ -26,6 +26,7 @@ class _AppState extends State<App> {
     await prefs.setBool('is_first_launch', false);
     print("This is Working");
     print("This is Working");
+    print("This is Working");
   }
 
   @override
