762e379dda2257d6e7e5887415b03a5c7f2cc0ab
Sayed Faisal Shah <142516641+SayedFaisalShah12@users.noreply.github.com>
2025-09-25 22:42:16 +0500

    update

--- lib/views/app.dart
+++ lib/views/app.dart
@@ -27,6 +27,7 @@ class _AppState extends State<App> {
     print('First launch complete');
     print('First launch complete');
     print('First launch complete');
+    print('First launch complete');
   }
 
   @override
