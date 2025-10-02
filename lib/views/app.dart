0bea90ca61fbef5c22349b18bc40389738f741be
Sayed Faisal Shah <142516641+SayedFaisalShah12@users.noreply.github.com>
2025-10-02 23:40:08 +0500

    update

--- lib/views/app.dart
+++ lib/views/app.dart
@@ -49,14 +49,6 @@ class _AppState extends State<App> with WidgetsBindingObserver {
     super.didChangeAppLifecycleState(state);
     if (state == AppLifecycleState.resumed) {
       _maybePromptBiometricOnResume();
-      print('resumed');
-      print('resumed');
-      print('resumed');
-      print('resumed');
-      print('resumed');
-      print('resumed');
-      print('resumed');
-      print('resumed');
     }
   }
 
